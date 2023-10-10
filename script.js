490a0e32107772d65a63dfe14687d36595405fc8
YordanDreick <131924250+YordanDreick@users.noreply.github.com>
2023-10-10 17:16:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.slider').slick({
+      autoplay: true, // Iniciar la reproducción automática
+      autoplaySpeed: 2000, // Velocidad de reproducción automática en milisegundos (2 segundos en este caso)
+      arrows: true, // Mostrar flechas de navegación
+      dots: true, // Mostrar indicadores de navegación
+    });
+  });
